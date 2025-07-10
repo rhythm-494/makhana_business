7c2fbeb23b0e5d2bd5da6cdfe7fa7fe00966f2e3
Rhythmsethiya <141202014+rhythm-494@users.noreply.github.com>
2025-07-10 22:31:30 +0530

    Create cloudinary.js

---
+++ nodejs_makhana/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dzwxehz0i',
+    api_key: '197155754195725',
+    api_secret: '**********'
+});
+
+module.exports = cloudinary;
